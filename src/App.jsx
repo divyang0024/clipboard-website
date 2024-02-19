17971e77eca147c1f923baae086feca9376b3cc0
Divyang <divyang0024@gmail.com>
2024-02-19 13:45:05 +0530

    images not rendering resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
 import "./index.css";
 import Logo from "../images/logo.svg";
+import imageComputer from "../images/image-computer.png";
+import imageDevices from "../images/image-devices.png";
+import iconBlacklist from "../images/icon-blacklist.svg";
+import iconText from "../images/icon-text.svg";
+import iconPreview from "../images/icon-preview.svg";
+import logoGoogle from "../images/logo-google.png";
+import logoIbm from "../images/logo-ibm.png";
+import logoMicrosoft from "../images/logo-microsoft.png";
+import logoHp from "../images/logo-hp.png";
+import logoVg from "../images/logo-vector-graphics.png";
+import iconFacebook from "../images/icon-facebook.svg";
+import iconTwitter from "../images/icon-twitter.svg";
+import iconInstagram from "../images/icon-instagram.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -37,10 +50,7 @@ function App() {
           </p>
           <div className="flex flex-col mt-24 md:flex-row gap-12 justify-center">
             <div className="md:flex md:items-center">
-              <img
-                src="../images/image-computer.png"
-                className="md:relative md:left-[-20%]"
-              />
+              <img src={imageComputer} className="md:relative md:left-[-20%]" />
             </div>
             <div className="flex flex-col justify-center gap-8">
               <div>
@@ -74,7 +84,7 @@ function App() {
             Whether you're on the go, or at your computer, you can access all
             your Clipboard snippets in a few simple clicks.
           </p>
-          <img src="../images/image-devices.png" className="mx-auto mt-8" />
+          <img src={imageDevices} className="mx-auto mt-8" />
         </div>
       </section>
 
@@ -88,7 +98,7 @@ function App() {
           </div>
           <div className="flex flex-col md:flex-row gap-10 md:justify-center">
             <div className="space-y-6">
-              <img src="../images/icon-blacklist.svg" className="mx-auto" />
+              <img src={iconBlacklist} className="mx-auto" />
               <h4 className="text-center">Create Blacklists</h4>
               <p className="snippetBoolets text-center">
                 Easily search your snippets by content, category, web address,
@@ -96,7 +106,7 @@ function App() {
               </p>
             </div>
             <div className="space-y-6">
-              <img src="../images/icon-text.svg" className="mx-auto" />
+              <img src={iconText} className="mx-auto" />
               <h4 className="text-center">Plain Text Snippets</h4>
               <p className="snippetBoolets text-center">
                 Remove unwanted formatting from copied text for a consistent
@@ -104,7 +114,7 @@ function App() {
               </p>
             </div>
             <div className="space-y-6">
-              <img src="../images/icon-preview.svg" className="mx-auto" />
+              <img src={iconPreview} className="mx-auto" />
               <h4 className="text-center">Sneak Preview</h4>
               <p className="snippetBoolets text-center">
                 Quick preview of all snippets on your Clipboard for easy access.
@@ -115,11 +125,11 @@ function App() {
       </section>
       <section className="companies">
         <div className="px-10 my-28 flex flex-col md:flex-row items-center justify-center gap-12 flex-wrap">
-          <img src="images/logo-google.png" alt="" />
-          <img src="images/logo-ibm.png" alt="" />
-          <img src="images/logo-microsoft.png" alt="" />
-          <img src="images/logo-hp.png" alt="" />
-          <img src="images/logo-vector-graphics.png" alt="" />
+          <img src={logoGoogle} alt="" />
+          <img src={logoIbm} alt="" />
+          <img src={logoMicrosoft} alt="" />
+          <img src={logoHp} alt="" />
+          <img src={logoVg} alt="" />
         </div>
       </section>
       <section>
@@ -143,7 +153,7 @@ function App() {
       <footer className="bg-gray-50">
         <div className="mt-28 px-10 py-10 flex flex-col md:flex-row gap-8 md:justify-between md:gap-8">
           <div>
-            <img src="../images/logo.svg" className="mx-auto w-[75px]" />
+            <img src={Logo} className="mx-auto w-[75px]" />
           </div>
           <div className="flex flex-col md:flex-row md:flex-wrap md:items-center gap-8 md:justify-center">
             <p className="snippetBoolets ficon">FAQs</p>
@@ -153,21 +163,9 @@ function App() {
             <p className="snippetBoolets ficon">Install Guide</p>
           </div>
           <div className="flex justify-center items-center gap-8">
-            <img
-              src="images/icon-facebook.svg"
-              alt=""
-              class="duration-200 ficon"
-            />
-            <img
-              src="images/icon-twitter.svg"
-              alt=""
-              class="duration-200 ficon"
-            />
-            <img
-              src="images/icon-instagram.svg"
-              alt=""
-              class="duration-200 ficon"
-            />
+            <img src={iconFacebook} alt="" class="duration-200 ficon" />
+            <img src={iconTwitter} alt="" class="duration-200 ficon" />
+            <img src={iconInstagram} alt="" class="duration-200 ficon" />
           </div>
         </div>
       </footer>
